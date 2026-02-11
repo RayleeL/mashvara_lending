6128de8e96588211d5fcc0ad6d5704f499d8afba
Амира Асанова <amiraasanova443@gmail.com>
2026-02-11 09:42:06 +0600

    feat: enlarge logo and add scroll-to-top functionality

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ const form = document.getElementById('contact-form');
 const submitBtn = document.getElementById('submit-btn');
 const alertBox = document.getElementById('form-alert');
 
-// Вставьте сюда ваш URL из Google Apps Script
+
 const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzwvKKR6TOkOARb1SPmFBOc9ymzIHgLoKPxmASzAkonEYuoCMzGfnmvpgu24lWJW6LV/exec';
 
 form.addEventListener('submit', e => {
   e.preventDefault();
   
-  // UI: Блокируем кнопку и меняем текст
+  
   submitBtn.disabled = true;
   submitBtn.innerText = 'Отправка...';
 
@@ -16,11 +16,9 @@ form.addEventListener('submit', e => {
 
   fetch(SCRIPT_URL, { method: 'POST', body: requestBody})
     .then(response => {
-      // Сброс формы и показ сообщения
       alertBox.classList.remove('hidden');
       form.reset();
       
-      // Возвращаем кнопку через 3 секунды
       setTimeout(() => {
         alertBox.classList.add('hidden');
         submitBtn.disabled = false;
